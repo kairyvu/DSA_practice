89b761bced25488dcf251a1ba2a3e82ca8eb78b8
Khoi Vu <kairyvd167@gmail.com>
2025-10-15 01:23:50 -0400

    Time: 1341 ms (74.84%) | Memory: 47.4 MB (10.32%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.py
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.py
@@ -5,13 +5,14 @@ class Solution:
         longest = 0
 
         for i in range(1, len(nums)):
-            if nums[i] <= nums[i - 1]:
+            if nums[i] > nums[i - 1]:
+                curr += 1
+            else:
                 res = max(res, min(prev, curr))
                 longest = max(curr, longest)
                 prev = curr
                 curr = 1
-                continue
-            curr += 1
-        longest = max(curr, longest)
+                
+        longestSplit = max(curr, longest) // 2
         res = max(res, min(prev, curr))
-        return res if longest // 2 < res else longest // 2
+        return res if longestSplit < res else longestSplit
