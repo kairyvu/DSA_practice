3670b33bdd900f9559c3447289c0a48d6b4bd9a1
Khoi Vu <kairyvd167@gmail.com>
2025-12-03 22:31:21 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (67.69%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordDict = set(wordDict)
+        n = len(s)
+
+        def backtrack(i):
+            if i == n:
+                return [""]
+            res = []
+            for j in range(i, n):
+                w = s[i:j+1]
+                if w not in wordDict:
+                    continue
+                stringList = backtrack(j + 1)
+                if not stringList:
+                    continue
+                for string in stringList:
+                    sentence = w
+                    if string:
+                        sentence += " " + string
+                    res.append(sentence)
+            return res
+        return backtrack(0)    
