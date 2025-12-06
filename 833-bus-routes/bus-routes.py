5cbe059d39e96bfa5fff9a37303c3a2e18166e2d
Khoi Vu <kairyvd167@gmail.com>
2025-12-05 22:17:24 -0500

    Time: 77 ms (87.15%) | Memory: 39.3 MB (85.79%) - LeetSync

--- 833-bus-routes/bus-routes.py
+++ 833-bus-routes/bus-routes.py
@@ -4,28 +4,29 @@ class Solution:
             return 0
 
         stopToBus = defaultdict(list)
-        busToStop = defaultdict(set)
-
         for i, route in enumerate(routes):
-            busToStop[i] = set(route)
             for stop in route:
                 stopToBus[stop].append(i)
 
-        q = deque([source])
-        visitedBus = set()
-        res = 0
+        q = deque()
+        visited = set()
+        for bus in stopToBus[source]:
+            q.append(bus)
+            visited.add(bus)
         
+        res = 1
+
         while q:
-            res += 1
-            n = len(q)
-            for _ in range(n):
-                currStop = q.popleft()
-                nextBuses = stopToBus[currStop]
-                for bus in nextBuses:
-                    if bus in visitedBus:
-                        continue
-                    visitedBus.add(bus)
-                    if target in busToStop[bus]:
+            for _ in range(len(q)):
+                currBus = q.popleft()
+                for stop in routes[currBus]:
+                    if stop == target:
                         return res
-                    q.extend(busToStop[bus])
+                    for nextBus in stopToBus[stop]:
+                        if nextBus in visited:
+                            continue
+                        visited.add(nextBus)
+                        q.append(nextBus)
+                    del stopToBus[stop]
+            res += 1
         return -1
