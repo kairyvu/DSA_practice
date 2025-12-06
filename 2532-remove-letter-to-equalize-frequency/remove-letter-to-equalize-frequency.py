d9a82ed63c68709dfc5ef39d732e2d00641e1718
Khoi Vu <kairyvd167@gmail.com>
2025-12-06 14:31:24 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (37.69%) - LeetSync

--- 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
@@ -7,7 +7,7 @@ class Solution:
         for c in freq:
             freq[c] -= 1
             freqs = [v for v in freq.values() if v > 0]
-            if not freqs or len(set(freqs)) == 1:
+            if len(set(freqs)) == 1:
                 return True
             freq[c] += 1
 
