f004033d8af14e322938ff3fa5dfae9f5cdd3bdf
Khoi Vu <kairyvd167@gmail.com>
2025-12-06 14:29:30 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.69%) - LeetSync

--- 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
@@ -4,7 +4,7 @@ class Solution:
         for c in word:
             freq[c] += 1
         
-        for c in freq.keys():
+        for c in freq:
             freq[c] -= 1
             freqs = [v for v in freq.values() if v > 0]
             if not freqs or len(set(freqs)) == 1:
