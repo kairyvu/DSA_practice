85b48bc9fe61838006b704d41c09b5016317a48e
Khoi Vu <kairyvd167@gmail.com>
2025-12-06 14:26:20 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (89.46%) - LeetSync

---
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
@@ -0,0 +1,14 @@
+class Solution:
+    def equalFrequency(self, word: str) -> bool:
+        freq = defaultdict(int)
+        for c in word:
+            freq[c] += 1
+        
+        for c in freq.keys():
+            freq[c] -= 1
+            freqs = [v for v in freq.values() if v > 0]
+            if not freqs or len(set(freqs)) == 1:
+                return True
+            freq[c] += 1
+
+        return False
