fc2ca3c73971d7ce28274c9376fde5d8c0f3a04a
Khoi Vu <kairyvd167@gmail.com>
2025-12-06 18:18:33 -0500

    Time: 93 ms (70.57%) | Memory: 34.4 MB (11.15%) - LeetSync

--- 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
@@ -1,10 +1,8 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
-        hsmap = defaultdict(int)
+        hsmap = {1: 0}
         
         def computeStep(num):
-            if num == 1:
-                return 0
             if num in hsmap:
                 return hsmap[num]
             hsmap[num] = 1 + computeStep(num // 2) if num % 2 == 0 else 1 + computeStep(3 * num + 1)
@@ -13,6 +11,6 @@ class Solution:
         computedArray = []
         for num in range(lo, hi + 1):
             computedArray.append((computeStep(num), num))
-        computedArray.sort()
+        computedArray.sort() # nlogn
         
         return computedArray[k - 1][1]
