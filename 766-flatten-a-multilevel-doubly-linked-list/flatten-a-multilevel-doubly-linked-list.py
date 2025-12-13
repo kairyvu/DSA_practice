6db6e9b4ffb362ec4758b1c5f3686b0ee365ffdb
Khoi Vu <kairyvd167@gmail.com>
2025-12-13 00:19:57 -0500

    Time: 43 ms (38.24%) | Memory: 18.2 MB (93.09%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.py
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.py
@@ -12,31 +12,27 @@ class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return head
-
+        
         def dfs(node):
             curr = node
             last = node
-
             while curr:
                 nextNode = curr.next
                 if curr.child:
-                    child_head = curr.child
-                    child_tail = dfs(child_head)
-
+                    curr.next = curr.child
+                    childTail = dfs(curr.child)
+                    curr.child.prev = curr
                     curr.child = None
-                    curr.next = child_head
-                    child_head.prev = curr
 
                     if nextNode:
-                        child_tail.next = nextNode
-                        nextNode.prev = child_tail
-
-                    last = child_tail
+                        childTail.next = nextNode
+                        nextNode.prev = childTail
+                    last = childTail
                     curr = nextNode
                 else:
                     last = curr
                     curr = nextNode
             return last
-
+        
         dfs(head)
         return head
