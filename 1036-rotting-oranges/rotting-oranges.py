5f8a2d052b4f980c46ada4cc52dff122df6c13a2
Khoi Vu <kairyvd167@gmail.com>
2025-12-13 19:55:16 -0500

    Time: 3 ms (81.36%) | Memory: 17.8 MB (43.87%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,18 +1,14 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         numOfFresh = 0
-        visited = set()
         q = deque()
 
         rows, cols = len(grid), len(grid[0])
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 0:
-                    continue
-                elif grid[r][c] == 2:
-                    visited.add((r, c))
+                if grid[r][c] == 2:
                     q.append((r, c))
-                else:
+                elif grid[r][c] == 1:
                     numOfFresh += 1
         
         dirs = [[0, 1], [1, 0], [-1, 0], [0, -1]]
@@ -23,9 +19,9 @@ class Solution:
                 grid[r][c] = 2
                 for dr, dc in dirs:
                     newR, newC = r + dr, c + dc
-                    if 0 <= newR < rows and 0 <= newC < cols and (newR, newC) not in visited and grid[newR][newC] == 1:
+                    if 0 <= newR < rows and 0 <= newC < cols and grid[newR][newC] == 1:
                         numOfFresh -= 1
-                        visited.add((newR, newC))
+                        grid[newR][newC] = 2
                         q.append((newR, newC))
             res += 1
         
