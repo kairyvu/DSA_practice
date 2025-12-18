a493867400b0fac3481a07a5f9c3bdc7abb5fbad
Khoi Vu <kairyvd167@gmail.com>
2025-12-18 17:12:33 -0500

    Time: 412 ms (92.94%) | Memory: 17.6 MB (99.27%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.py
+++ 611-valid-triangle-number/valid-triangle-number.py
@@ -3,12 +3,12 @@ class Solution:
         n, res = len(nums), 0
         nums.sort()
 
-        for i in range(n - 2):
-            if nums[i] == 0:
-                continue
-            largestEdge = i + 2
-            for j in range(i + 1, n - 1):
-                while largestEdge < n and nums[i] + nums[j] > nums[largestEdge]:
-                    largestEdge += 1
-                res += largestEdge - j - 1
+        for i in range(n - 1, -1, -1):
+            left, right = 0, i - 1
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    res += right - left
+                    right -= 1
+                else:
+                    left += 1
         return res
