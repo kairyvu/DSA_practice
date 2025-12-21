206de70064a3c6c071738db6a214fd1e5550c244
Khoi Vu <kairyvd167@gmail.com>
2025-12-21 00:28:46 -0500

    Time: 3 ms (24.56%) | Memory: 17.6 MB (97.41%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid) - 1, len(obstacleGrid[0]) - 1
+        memo = {}
+
+        def dfs(r, c):
+            if not (0 <= r <= m and 0 <= c <= n) or obstacleGrid[r][c] == 1:
+                return 0
+            if r == m and c == n:
+                return 1
+            if (r, c) in memo:
+                return memo[(r, c)]
+            memo[(r, c)] = dfs(r + 1, c) + dfs(r, c + 1)
+            return memo[(r, c)]
+        
+        return dfs(0, 0)
