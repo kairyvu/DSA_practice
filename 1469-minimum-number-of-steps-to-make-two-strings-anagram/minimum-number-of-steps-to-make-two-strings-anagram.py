e7fcd22f9011d3ebeb7a628dc9b2cc2c35c0bc45
Khoi Vu <kairyvd167@gmail.com>
2025-12-23 21:12:19 -0500

    Time: 183 ms (5.10%) | Memory: 17.9 MB (78.59%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -6,4 +6,7 @@ class Solution:
             if c not in counterS or counterS[c] == 0:
                 continue
             counterS[c] -= 1
-        return sum(counterS.values())
+        res = 0
+        for c in counterS:
+            res += counterS[c]
+        return res
