b53205b6dbf6ccedb42d0c3264781e8e8206fd67
Khoi Vu <kairyvd167@gmail.com>
2025-12-24 10:46:48 -0500

    Time: 3 ms (81.31%) | Memory: 17.4 MB (97.04%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/add-two-numbers-ii.py
@@ -8,22 +8,19 @@ class Solution:
         stack1, stack2 = [], []
         
         while l1:
-            stack1.append(l1)
+            stack1.append(l1.val)
             l1 = l1.next
         
         while l2:
-            stack2.append(l2)
+            stack2.append(l2.val)
             l2 = l2.next
         
         extra = 0
         prevNode = None
         
         while stack1 or stack2 or extra:
-            num1, num2 = 0, 0
-            if stack1:
-                num1 = stack1.pop().val
-            if stack2:
-                num2 = stack2.pop().val
+            num1 = stack1.pop() if stack1 else 0
+            num2 = stack2.pop() if stack2 else 0
             
             total = num1 + num2 + extra
             extra = total // 10
